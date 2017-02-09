08f7dc310d074f93e74249219b2996e83dc9ce0b
pdarde-daitan <pdarde@daitangroup.com>
2017-02-09 15:07:55 -0200

    App 681 - Create 3rd party IntegrationHeader component (#10)
    * APP-681, Integration Header Component
    
    * APP-681, fix styles to rem

---
+++ integration-fe/src/components/IntegrationHeader/IntegrationHeader.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-debugger */
+import React from 'react';
+import config from '../../js/config.service';
+import './styles/styles.less';
+
+const IntegrationHeader = () => (
+  <div className='wrapper integration-header'>
+    <header>
+      <figure>
+        <img src={`../../img/${config.logo}`} alt={config.app_name} />
+      </figure>
+      <h2>{config.app_name}</h2>
+    </header>
+  </div>
+);
+
+export default IntegrationHeader;
--- integration-fe/src/views/Home.jsx
+++ integration-fe/src/views/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import TableInstanceContainer from '../containers/TableInstanceContainer';
+import IntegrationHeader from '../components/IntegrationHeader/IntegrationHeader';
 
 const Home = () => (
   <div>
+    <IntegrationHeader />
     <TableInstanceContainer />
   </div>
 );
