cc36b53935a8bd4bed9b110903334f3c68cdd2a8
Evandro Carenho <ecarrenho@daitangroup.com>
2017-03-08 16:45:54 -0300

    Moving Configurator Apps refactoring to a feature branch.

--- integration-fe/src/js/config.service.js
+++
@@ -1,7 +0,0 @@
-export default ({
-  app_title: 'Configure JIRA',
-  app_name: 'JIRA',
-  IM_shorthand: 'JIRA', // will display "One on One with JIRA"
-  toogleSetupInstructions: true,
-  logo: 'jira_logo.png',
-});
