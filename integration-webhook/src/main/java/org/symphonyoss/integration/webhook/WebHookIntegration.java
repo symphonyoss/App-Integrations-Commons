8e7508272aa468d776a23f4a1c97f2234c81378d
Evandro Carrenho <ecarrenho@daitangroup.com>
2017-04-19 09:56:57 -0300

    Merge pull request #39 from rsanchez-daitan/879_Return_200_when_receiving_500_from_agent
    Corrects return of API

--- integration-webhook/src/main/java/org/symphonyoss/integration/webhook/WebHookIntegration.java
+++ integration-webhook/src/main/java/org/symphonyoss/integration/webhook/WebHookIntegration.java
@@ -241,7 +241,7 @@ public abstract class WebHookIntegration extends BaseIntegration {
    * @throws WebHookParseException
    */
   public void handle(String instanceId, String integrationUser, WebHookPayload input)
-      throws WebHookParseException {
+      throws WebHookParseException, RemoteApiException {
     if (isAvailable()) {
       IntegrationInstance instance = getIntegrationInstance(instanceId);
       Message message = parseRequest(instance, integrationUser, input);
@@ -284,7 +284,8 @@ public abstract class WebHookIntegration extends BaseIntegration {
    * @param payload Json Node that contains a list of streams
    * @throws IOException Reports failure to validate the payload
    */
-  public void welcome(IntegrationInstance instance, String integrationUser, String payload) {
+  public void welcome(IntegrationInstance instance, String integrationUser, String payload)
+      throws RemoteApiException {
     List<String> instanceStreams = streamService.getStreams(instance);
     List<String> streams = streamService.getStreams(payload);
 
@@ -360,7 +361,7 @@ public abstract class WebHookIntegration extends BaseIntegration {
    * @param message Formatted MessageML
    */
   private void postMessage(IntegrationInstance instance, String integrationUser,
-      List<String> streams, Message message) {
+      List<String> streams, Message message)  throws RemoteApiException{
     // Post a message
     List<Message> response = sendMessage(instance, integrationUser, streams, message);
     // Get the timestamp of the last message posted
@@ -381,7 +382,7 @@ public abstract class WebHookIntegration extends BaseIntegration {
    * @return List of messages posted to the streams.
    */
   private List<Message> sendMessage(IntegrationInstance instance, String integrationUser,
-      List<String> streams, Message message) {
+      List<String> streams, Message message) throws RemoteApiException {
     List<Message> response = new ArrayList<>();
 
     try {
