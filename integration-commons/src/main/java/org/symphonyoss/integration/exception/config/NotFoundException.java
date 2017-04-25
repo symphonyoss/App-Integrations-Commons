6e8234b3db37f93380cbf690f1d94b3056f0601c
Evandro Carrenho <ecarrenho@daitangroup.com>
2017-04-24 21:28:18 -0300

    Merge pull request #41 from rsanchez-daitan/APP-871_Deactivated_webhook_log
    APP-871 Return 404 when webhook is deactivated

---
+++ integration-commons/src/main/java/org/symphonyoss/integration/exception/config/NotFoundException.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2016-2017 Symphony Integrations - Symphony LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.symphonyoss.integration.exception.config;
+
+/**
+ * Returned when trying to post message but not exists configuration.
+ *
+ * Created by Cassiano Repache on 12/04/17.
+ */
+public class NotFoundException extends RemoteConfigurationException {
+
+  public NotFoundException() {
+    super();
+  }
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+
+  public NotFoundException(Throwable cause) {
+    super(cause);
+  }
+
+}
