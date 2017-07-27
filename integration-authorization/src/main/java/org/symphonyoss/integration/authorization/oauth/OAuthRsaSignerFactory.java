c6f1233699a2636af6d0a68b1efd2d2e34d86b76
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-07-27 16:54:47 -0300

    APP-1215 Retrieve public key

--- integration-authorization/src/main/java/org/symphonyoss/integration/authorization/oauth/OAuthRsaSignerFactory.java
+++ integration-authorization/src/main/java/org/symphonyoss/integration/authorization/oauth/OAuthRsaSignerFactory.java
@@ -7,8 +7,10 @@ import org.symphonyoss.integration.authorization.oauth.v1.OAuth1Exception;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * Builds a RSA Signer based on a PKCS8 private key.
@@ -30,6 +32,7 @@ public final class OAuthRsaSignerFactory {
   private static PrivateKey getPrivateKey(String privateKey) {
     byte[] privateBytes = Base64.decodeBase64(privateKey);
     PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateBytes);
+
     try {
       KeyFactory kf = KeyFactory.getInstance("RSA");
       return kf.generatePrivate(keySpec);
@@ -40,6 +43,22 @@ public final class OAuthRsaSignerFactory {
     } catch (InvalidKeySpecException e) {
       throw new OAuth1Exception("Informed private key is invalid.", e,
           "Generate the private key again, use the PKCS8 syntax.");
+    }
+  }
+
+  public PublicKey getPublicKey(String publicKey) {
+    try {
+      byte[] encoded = Base64.decodeBase64(publicKey);
+      X509EncodedKeySpec spec = new X509EncodedKeySpec(encoded);
+
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      return kf.generatePublic(spec);
+    } catch (NoSuchAlgorithmException e) {
+      throw new OAuth1Exception("'RSA' algorithm not found.", e,
+          "Verify the available signature algorithms supported by your Java installation. "
+              + "There must be one called 'RSA'.");
+    } catch (InvalidKeySpecException e) {
+      throw new OAuth1Exception("Informed public key is invalid.", e, "Generate the public key again.");
     }
   }
 }
