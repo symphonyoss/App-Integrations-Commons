b4195c33b349e58d0b1a21603ab477caf89ced4b
Hamitay <henriqueamitay@gmail.com>
2017-08-17 10:30:55 -0300

    APP-1179/APP-1180 Added OAuth1HttpRequestExceptionTest to achieve 90% code coverage

---
+++ integration-authorization/src/test/java/org/symphonyoss/integration/authorization/oauth/v1/OAuth1HttpRequestExceptionTest.java
@@ -0,0 +1,39 @@
+package org.symphonyoss.integration.authorization.oauth.v1;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.api.client.http.HttpResponse;
+import org.junit.Test;
+import org.symphonyoss.integration.exception.ExpectedMessageBuilder;
+
+/**
+ * Unit tests for {@link OAuth1HttpRequestException}
+ * Created by hamitay on 8/17/17.
+ */
+
+public class OAuth1HttpRequestExceptionTest {
+
+  private static final String COMPONENT = "Third-party integration";
+
+  private static final String MESSAGE = "message";
+
+  private static final String NO_SOLUTION =
+      "No solution has been cataloged for troubleshooting this problem.";
+
+  private static final int CODE = 400;
+
+  @Test
+  public void testOAuth1HttpRequestException() {
+    OAuth1HttpRequestException exception = new OAuth1HttpRequestException("message", 400);
+    String resultMessage = exception.getMessage();
+    String expectedMessage =
+        new ExpectedMessageBuilder().component(COMPONENT)
+            .message(MESSAGE)
+            .solutions(NO_SOLUTION)
+            .build();
+    int resultCode = exception.getCode();
+
+    assertEquals(expectedMessage, resultMessage);
+    assertEquals(CODE, resultCode);
+  }
+}
