9e3b5aab051e838b87c90bd42ecdf90c41feab00
campidelli <campidelli@gmail.com>
2017-08-22 19:16:34 -0300

    APP-1206: Conflicts fixes.

--- integration-authentication-api/src/main/java/org/symphonyoss/integration/authentication/api/jwt/JwtAuthentication.java
+++ integration-authentication-api/src/main/java/org/symphonyoss/integration/authentication/api/jwt/JwtAuthentication.java
@@ -16,6 +16,8 @@
 
 package org.symphonyoss.integration.authentication.api.jwt;
 
+import org.symphonyoss.integration.authentication.api.model.JwtPayload;
+
 /**
  * Service interface responsible for handling JWT authentication stuff.
  *
@@ -25,19 +27,21 @@ public interface JwtAuthentication {
 
   /**
    * Return user identifier from HTTP Authorization header.
-   * @param authorizationHeader HTTP Authorization header
+   * @param configurationId Configuration ID.
+   * @param authorizationHeader HTTP Authorization header.
    * @return User identifier or null if the authorization header is not present or it's not a valid
    * JWT token
    */
-  Long getUserIdFromAuthorizationHeader(String authorizationHeader);
+  Long getUserIdFromAuthorizationHeader(String configurationId, String authorizationHeader);
 
   /**
    * Retrieves JWT token from HTTP Authorization header.
+   * @param configurationId Configuration ID.
    * @param authorizationHeader HTTP Authorization header
    * @return JWT token or null if the authorization header is not present or it's not a valid JWT
    * token
    */
-  String getJwtToken(String authorizationHeader);
+  JwtPayload getJwtToken(String configurationId, String authorizationHeader);
 
   /**
    * Start the JWT authentication between the App and the SBE.
@@ -54,4 +58,12 @@ public interface JwtAuthentication {
    */
   boolean isValidTokenPair(String configurationId, String applicationToken,
       String symphonyToken);
+
+  /**
+   * Validate if the sent JWT is valid by checking its signer and decodification and then return it.
+   * @param configurationId Application identifier.
+   * @param jwt Json Web Token containing the user/app authentication data.
+   * @return JwtPayload parsed;
+   */
+  JwtPayload parseJwtPayload(String configurationId, String jwt);
 }
