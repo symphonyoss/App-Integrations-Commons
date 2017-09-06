85a61ceb9a38de39161e003e3534cbd4a8da10ca
campidelli <campidelli@gmail.com>
2017-09-06 17:56:56 -0300

    APP-1175: Cryptography service - initial version.

---
+++ integration-commons/src/main/java/org/symphonyoss/integration/exception/CryptoRuntimeException.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2016-2017 Symphony Integrations - Symphony LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.symphonyoss.integration.exception;
+
+/**
+ * Unchecked exception thrown to indicate there is a problem with the attempt of encrypt or decrypt
+ * a text.
+ * Created by campidelli on 05-sep-17.
+ */
+public class CryptoRuntimeException extends IntegrationRuntimeException {
+
+  private static final String COMPONENT = "Cryptography service";
+
+  public CryptoRuntimeException(String message, String... solutions) {
+    super(COMPONENT, message, solutions);
+  }
+
+  public CryptoRuntimeException(String message, Throwable cause, String... solutions) {
+    super(COMPONENT, message, cause, solutions);
+  }
+}
---
+++ integration-commons/src/main/java/org/symphonyoss/integration/service/CryptoService.java
@@ -0,0 +1,28 @@
+package org.symphonyoss.integration.service;
+
+/**
+ * Contract to define responsibilities to provide cryptography services.
+ * Created by campidelli on 9/5/17.
+ */
+public interface CryptoService {
+
+  /**
+   * Encrypt the given text using the given key.
+   * @param plainText Text to be encrypted.
+   * @param key Key used as secret to encrypt te text.
+   * @return Encrypted text.
+   */
+  String encrypt(String plainText, String key);
+
+  /**
+   * Decrypt the given text using the given key.
+   * @param encryptedText Text to be decrypted.
+   * @param key Key used as secret to decrypt te text.
+   * @return Decrypted text.
+   */
+  String decrypt(String encryptedText, String key);
+
+  String encrypt(String configurationId, String plainText, boolean b);
+
+  String decrypt(String configurationId, String plainText, boolean b);
+}
