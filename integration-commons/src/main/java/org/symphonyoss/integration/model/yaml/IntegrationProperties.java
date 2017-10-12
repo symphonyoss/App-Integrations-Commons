001a467f1417bee2def52667e8e71d2b89e5f54b
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-10-12 18:31:48 -0300

    APP-1300: Healtchcheck timeout is now configurable

--- integration-commons/src/main/java/org/symphonyoss/integration/model/yaml/IntegrationProperties.java
+++ integration-commons/src/main/java/org/symphonyoss/integration/model/yaml/IntegrationProperties.java
@@ -51,7 +51,7 @@ public class IntegrationProperties {
 
   private ConnectionInfo keyManagerAuth;
 
-  private ApiClientConfig apiClientConfig;
+  private HttpClientConfig httpClientConfig;
 
   private Map<String, Application> applications = new HashMap<>();
 
@@ -108,12 +108,12 @@ public class IntegrationProperties {
   }
 
 
-  public ApiClientConfig getApiClientConfig() {
-    return apiClientConfig;
+  public HttpClientConfig getHttpClientConfig() {
+    return httpClientConfig;
   }
 
-  public void setApiClientConfig(ApiClientConfig apiClientConfig) {
-    this.apiClientConfig = apiClientConfig;
+  public void setHttpClientConfig(HttpClientConfig httpClientConfig) {
+    this.httpClientConfig = httpClientConfig;
   }
 
   public Map<String, Application> getApplications() {
