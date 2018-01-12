80c55b520e8355bd434e896b82a0cab3cb1c5fc1
Luana Pimentel <lpimentel@daitangroup.com>
2018-01-12 17:45:34 -0200

    APP-1551 - Removed wrong service names

--- integration-authentication-api/src/main/java/org/symphonyoss/integration/authentication/api/enums/ServiceName.java
+++ integration-authentication-api/src/main/java/org/symphonyoss/integration/authentication/api/enums/ServiceName.java
@@ -4,5 +4,5 @@ package org.symphonyoss.integration.authentication.api.enums;
  * Created by hamitay on 10/01/18.
  */
 public enum ServiceName {
-  POD_SESSION_MANAGER, AGENT, INTEGRATION_API, KEY_MANAGER_AUTH, POD, SYMPHONY_API, KEY_MANAGER;
+  POD_SESSION_MANAGER, AGENT, KEY_MANAGER_AUTH, POD, KEY_MANAGER
 }
